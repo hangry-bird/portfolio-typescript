b22bc810eadb0b44f6810ee7cd4f453a18f17e63
hangry-bird <starcjs7@gmail.com>
2022-02-03 17:52:40 +0900

    이력서 페이지 스킬 부분 수정 중

--- src/domains/resume/Container/resumeTitle.tsx
+++ src/domains/resume/Container/resumeTitle.tsx
@@ -5,8 +5,8 @@ const ResumeTitle = () => {
 
   return (
     <div className="resume-container title">
-      <h1>OOO OOOO OOO</h1>
-      <h1>OOOOOO.</h1>
+      <h1>개발을 재밌어 하는</h1>
+      <h1>개발자입니다.</h1>
     </div>
   );
 }
